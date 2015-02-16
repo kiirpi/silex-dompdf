bfa920b957fa5ae484177c10e8db2fc8f3ad3efb
Bill'O <ateilhet@gmail.com>
2015-02-16 14:52:03 +0100

    initial commit

---
+++ src/Dompdf/DompdfServiceProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Dompdf;
+
+// disable DOMPDF's internal autoloader if you are using Composer
+define('DOMPDF_ENABLE_AUTOLOAD', false);
+
+// include DOMPDF's default configuration
+require_once '../../vendor/dompdf/dompdf/dompdf_config.inc.php';
+
+use Silex\Application;
+use Silex\ServiceProviderInterface;
+
+/**
+ * Class DompdfServiceProvider
+ * @package Dompdf
+ * @author Bill'O <ateilhet@gmail.com>
+ */
+class DompdfServiceProvider implements ServiceProviderInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function register(Application $app)
+    {
+        $app['dompdf'] = $app->share(function () use ($app) {
+                return new \DOMPDF();
+            });
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    // @codeCoverageIgnoreStart
+    public function boot(Application $app)
+    {
+    }
+    // @codeCoverageIgnoreEnd
+}
